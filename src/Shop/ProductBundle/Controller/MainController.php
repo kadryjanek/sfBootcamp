db44a66b603d8902c335faa186a7198078bd0232
Mateusz Waszczuk <mateuszwaszczuk@outlook.com>
2014-11-05 19:20:37 +0100

    Formularz kontaktowy

--- src/Shop/ProductBundle/Controller/MainController.php
+++ src/Shop/ProductBundle/Controller/MainController.php
@@ -80,6 +80,31 @@ class MainController extends Controller
         
         $form = $this->createForm(new ContactFormType());
         
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $data = $form->getData();
+            
+            // name - $data['name']
+            
+            $message = \Swift_Message::newInstance()
+                    ->setSubject('Wiadomość z serwisu ShopApp')
+                    ->setFrom($data['email'])
+                    ->setTo('symfony.app@o2.pl')
+                    ->setBody(
+                            $this->renderView('ShopProductBundle:Main:email.html.twig', [
+                                'name' => $data['name'],
+                                'message' => $data['message'],
+                            ]), 'text/html');
+            
+            if ($this->get('mailer')->send($message)) {
+                $this->get('session')->getFlashBag()->add('success', 'Wiadomość została wysłana');
+            } else {
+                $this->get('session')->getFlashBag()->add('error', 'Błąd wysyłki');
+            }
+            
+            return $this->redirect($this->generateUrl('contact'));            
+        }
+        
         return $this->render('ShopProductBundle:Main:contact.html.twig', [
            'form' => $form->createView(), 
         ]);

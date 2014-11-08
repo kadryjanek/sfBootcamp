c64eb4f4bd9d114bfcf1dfe8dfd6a67546e88117
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-11-08 14:47:29 +0100

    Form validation added

--- src/Shop/ProductBundle/Form/ContactFormType.php
+++ src/Shop/ProductBundle/Form/ContactFormType.php
@@ -3,6 +3,8 @@ namespace Shop\ProductBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class ContactFormType  extends AbstractType
 {
@@ -13,19 +15,22 @@ class ContactFormType  extends AbstractType
                 'label' => 'Imię i nazwisko',
                 'attr' => [
                     'class' => 'form-control',
-                ]
+                ],
+                'constraints'   => new NotBlank()
             ])
             ->add('email', 'email', [
                 'label' => 'Email',
                 'attr' => [
                     'class' => 'form-control',
-                ]
+                ],
+                'constraints'   => [new NotBlank(), new Email()]
             ])
             ->add('message', 'textarea', [
                 'label' => 'Wiadomość',
                 'attr' => [
                     'class' => 'form-control',
-                ]
+                ],
+                'constraints'   => new NotBlank()
             ])
         ;
     }

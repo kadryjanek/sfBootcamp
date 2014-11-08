6f331f842c695516583cac5a100c8fb8d12e27d0
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-11-08 16:22:33 +0100

    Ajax form handling added

--- app/Resources/public/js/scripts.js
+++ app/Resources/public/js/scripts.js
@@ -1,16 +1,37 @@
 // gdy dokument zostanie w pełni załadowany
-$(document).ready(function(){
+$(document).ready(function() {
 	
 	// formularz kontaktowy
-	$form = $('form#contact-form'); 
+	var $form = $('form#contact-form');
+	// akcja dla formularza kontaktowego
+	var action = $form.attr('action');
 	
-	// akcja wykonywana w momencie kliknięcia przycisku 'submit'
-	$form.submit(function() {
+	var handleForm = function() {
 
-		console.log("Send form");
+		// akcja ajaksowa metodą POST
+		$.post(
+			action, 
+			$form.serialize(),
+			function(response) {
+			
+				if (response.success) {
+					alert(response.message)
+				} else {
+					alert(response.message);
+					$form.replaceWith(response.view);
+				}
+				
+				$form = $('form#contact-form');
+				$form.submit(handleForm);
+			}, 
+			'json'
+		);
 		
 		// zapobiega standardowemu wysłaniu formularza
 		return false;
-	})
+	}
+	
+	// akcja wykonywana w momencie kliknięcia przycisku 'submit'
+	$form.submit(handleForm);
 	
 });
--- src/Shop/ProductBundle/Controller/MainController.php
+++ src/Shop/ProductBundle/Controller/MainController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Doctrine\Common\Collections\ArrayCollection;
 use Shop\ProductBundle\Form\ContactFormType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MainController extends Controller
 {
@@ -87,14 +89,24 @@ class MainController extends Controller
             // name - $data['name']
             
             $message = \Swift_Message::newInstance()
-                    ->setSubject('Wiadomość z serwisu ShopApp')
-                    ->setFrom($data['email'])
-                    ->setTo('symfony.app@o2.pl')
-                    ->setBody(
-                            $this->renderView('ShopProductBundle:Main:email.html.twig', [
-                                'name' => $data['name'],
-                                'message' => $data['message'],
-                            ]), 'text/html');
+                ->setSubject('Wiadomość z serwisu ShopApp')
+                ->setFrom($data['email'])
+                ->setTo('symfony.app@o2.pl')
+                ->setBody(
+                    $this->renderView('ShopProductBundle:Main:email.html.twig', [
+                        'name' => $data['name'],
+                        'message' => $data['message'],
+                    ]), 
+                'text/html');
+            
+            // jeśli to jest żądanie Ajaksowe
+            if ($request->isXmlHttpRequest()) {
+                
+                return new JsonResponse([
+                    'success'   => true,
+                    'message'   => 'Formularz został pomyślnie wysłany'
+                ]);
+            }
             
             if ($this->get('mailer')->send($message)) {
                 $this->get('session')->getFlashBag()->add('success', 'Wiadomość została wysłana');
@@ -103,6 +115,18 @@ class MainController extends Controller
             }
             
             return $this->redirect($this->generateUrl('contact'));            
+        }
+        
+        // jeśli to jest żądanie Ajaksowe
+        if ($request->isXmlHttpRequest()) {
+            
+            return new JsonResponse([
+                'success'   => false, 
+                'message'   => 'W formularzu wystąpiły błędy opisane szczegółowo poniżej.',
+                'view'      => $this->renderView('ShopProductBundle:Main:form.html.twig', [
+                    'form'  => $form->createView()
+                ])
+            ]);
         }
         
         return $this->render('ShopProductBundle:Main:contact.html.twig', [
